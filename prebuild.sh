cb280a9e78d665266afebbc1dce8c4838c888d40
Julien Thomas <jthomas@zenetys.com>
2020-12-07 04:33:33 +0100

    BUILD: Fix el6 build, use archive repositories since EOL

--- prebuild.sh
+++ prebuild.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -xe
 
-if [[ $DIST_TYPE == el ]]; then
+if [[ $DIST == el6 ]]; then
     # EPEL6: libwebp
+    build_dl https://archives.fedoraproject.org/pub/archive/epel/6/x86_64/epel-release-6-8.noarch.rpm
+    rpm -Uvh "$CACHEDIR/epel-release-6-8.noarch.rpm"
+    # el6 EOL, use archive repositories
+    sed -i -re 's,mirror\.centos\.org,vault.centos.org,; s,^(mirrorlist),#\1,; s,^#(baseurl),\1,' \
+        /etc/yum.repos.d/CentOS-Base.repo
+
+elif [[ $DIST_TYPE == el ]]; then
     # EPEL7: libtelnet, libuv, libwebsockets
     # EPEL8: libssh2
     build_dl "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$DIST_VERSION.noarch.rpm"
