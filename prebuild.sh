4b9a64c1a81310242db71e12d0831a47aecf938d
Julien Thomas <jthomas@zenetys.com>
2025-06-24 10:22:54 +0200

    BUILD: Missing old update of prebuild.sh since el6 support was droped

--- prebuild.sh
+++ prebuild.sh
@@ -1,16 +1,11 @@
 #!/bin/bash -xe
 
-if [[ $DIST == el6 ]]; then
-    # EPEL6: libwebp
-    build_dl https://archives.fedoraproject.org/pub/archive/epel/6/x86_64/epel-release-6-8.noarch.rpm
-    rpm -Uvh "$CACHEDIR/epel-release-6-8.noarch.rpm"
-    # el6 EOL, use archive repositories
-    sed -i -re 's,mirror\.centos\.org,vault.centos.org,; s,^(mirrorlist),#\1,; s,^#(baseurl),\1,' \
-        /etc/yum.repos.d/CentOS-Base.repo
-
-elif [[ $DIST_TYPE == el ]]; then
+if [[ $DIST_TYPE == el && $DIST_VERSION == [89] ]]; then
     # EPEL7: libtelnet, libuv, libwebsockets
     # EPEL8: libssh2
     build_dl "https://dl.fedoraproject.org/pub/epel/epel-release-latest-$DIST_VERSION.noarch.rpm"
     rpm -Uvh "$CACHEDIR/epel-release-latest-$DIST_VERSION.noarch.rpm"
+else
+    echo "FATAL: Unsupported build target, DIST=$DIST"
+    exit 2
 fi
